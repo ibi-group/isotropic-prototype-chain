0a862f00a876cf12a0814764efd0858be953dfaf
Steven Olmsted <steven.olmsted@ibigroup.com>
2021-02-22 00:56:40 -0500

    Implement isotropic-dev-dependencies

--- test/prototype-chain.js
+++ test/prototype-chain.js
@@ -1,5 +1,5 @@
-import _chai from 'chai';
-import _mocha from 'mocha';
+import _chai from 'isotropic-dev-dependencies/lib/chai.js';
+import _mocha from 'isotropic-dev-dependencies/lib/mocha.js';
 import _prototypeChain from '../js/prototype-chain.js';
 
 _mocha.describe('prototype-chain', () => {
